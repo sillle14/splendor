51e1beb214f4bca9fa822aeb93f870f0514202a3
Lewis Silletto <lsilletto@caredash.com>
2018-11-22 08:36:18 -0500

    pep8

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,12 +1,13 @@
 # Updated Animation Starter Code
-import sys
-sys.path.append('..')
+import random
+
 from core.game_state import GameState
 from tkinter import *
 from GUI.widgets import *
 from GUI.game_pieces import *
 
-import random
+sys.path.append('..')
+
 
 ####################################
 # customize these functions
@@ -14,45 +15,49 @@ import random
 
 def init(data, names):
     # load data.xyz as appropriate
-    updateGame(data, GameState(names))
+    update_game(data, GameState(names))
 
 
-def updateGame(data, game_state):
+def update_game(data, game_state):
     data.game = game_state
 
 
-def mousePressed(event, data):
+def mouse_pressed(event, data):
     # use event.x and event.y
     pass
 
-def keyPressed(event, data):
+
+def key_pressed(event, data):
     # use event.char and event.keysym
     if event.keysym == "n":
         newstates = data.game.get_possible_moves()[0]
         random.shuffle(newstates)
         newstate = newstates[0]
-        updateGame(data, newstate)
+        update_game(data, newstate)
         print(data.game)
 
-def timerFired(data):
+
+def timer_fired(data):
     pass
 
-def redrawAll(canvas, data):
+
+def redraw_all(canvas, data):
     # draw cards
     for i in range(3):
         for j in range(4):
-            drawCard(canvas, data.game.display[i*4+j],100+  j*135,100 + (2-i)*210)
+            draw_card(canvas, data.game.display[i * 4 + j], 100 + j * 135, 100 + (2 - i) * 210)
     # draw display gems
     for (i, gem) in enumerate(Gem):
         for j in range(data.game.gems.amount(gem)):
-            drawGem(canvas, gem, 700 + j*10, 150 + 110*i)
+            draw_gem(canvas, gem, 700 + j * 10, 150 + 110 * i)
     # draw players
     for (i, player) in enumerate(data.game.players):
-        drawPlayer(canvas, player, 800, i * 200)
+        draw_player(canvas, player, 800, i * 200)
     # draw other
     canvas.create_text(0 + 20, 0 + 20, text="Splendor", anchor="nw", font="Arial 40 bold")
-    canvas.create_text(0 + 20, data.height - 20, text="Turn: "+str(data.game.turns), anchor="sw", font="Arial 40  bold")
-    
+    canvas.create_text(0 + 20, data.height - 20, text="Turn: " + str(data.game.turns), anchor="sw",
+                       font="Arial 40  bold")
+
 
 ####################################
 # use the run function as-is
@@ -61,45 +66,50 @@ def redrawAll(canvas, data):
 def run(names):
     width = 1200
     height = 800
-    def redrawAllWrapper(canvas, data):
+
+    def redraw_all_wrapper(canvas, data):
         canvas.delete(ALL)
         canvas.create_rectangle(0, 0, data.width, data.height,
                                 fill='beige', width=0)
-        redrawAll(canvas, data)
+        redraw_all(canvas, data)
         canvas.update()
 
-    def mousePressedWrapper(event, canvas, data):
-        mousePressed(event, data)
-        redrawAllWrapper(canvas, data)
+    def mouse_pressed_wrapper(event, canvas, data):
+        mouse_pressed(event, data)
+        redraw_all_wrapper(canvas, data)
 
-    def keyPressedWrapper(event, canvas, data):
-        keyPressed(event, data)
-        redrawAllWrapper(canvas, data)
+    def key_pressed_wrapper(event, canvas, data):
+        key_pressed(event, data)
+        redraw_all_wrapper(canvas, data)
 
-    def timerFiredWrapper(canvas, data):
-        timerFired(data)
-        redrawAllWrapper(canvas, data)
+    def timer_fired_wrapper(canvas, data):
+        timer_fired(data)
+        redraw_all_wrapper(canvas, data)
         # pause, then call timerFired again
-        canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+        canvas.after(data.timerDelay, timer_fired_wrapper, canvas, data)
+
     # Set up data and call init
-    class Struct(object): pass
+    class Struct(object):
+        pass
+
     data = Struct()
     data.width = width
     data.height = height
-    data.timerDelay = 100 # milliseconds
+    data.timerDelay = 100  # milliseconds
     root = Tk()
-    root.resizable(width=False, height=False) # prevents resizing window
+    root.resizable(width=False, height=False)  # prevents resizing window
     init(data, names)
+
     # create the root and the canvas
     canvas = Canvas(root, width=data.width, height=data.height)
     canvas.configure(bd=0, highlightthickness=0)
     canvas.pack()
+
     # set up events
-    root.bind("<Button-1>", lambda event:
-                            mousePressedWrapper(event, canvas, data))
-    root.bind("<Key>", lambda event:
-                            keyPressedWrapper(event, canvas, data))
-    timerFiredWrapper(canvas, data)
+    root.bind("<Button-1>", lambda event: mouse_pressed_wrapper(event, canvas, data))
+    root.bind("<Key>", lambda event: key_pressed_wrapper(event, canvas, data))
+    timer_fired_wrapper(canvas, data)
+
     # and launch the app
     root.mainloop()  # blocks until window is closed
     print("bye!")
--- GUI/widgets.py
+++ GUI/widgets.py
@@ -1,6 +1,7 @@
 # Basic widgets for tkinter
 
-class Button(object) : 
+
+class Button(object):
 
     def __init__(self, x0, y0, x1, y1, **kwargs):
         self.x0 = x0
@@ -17,9 +18,8 @@ class Button(object) :
             self.fill = "white"
 
     def draw(self, canvas):
-        canvas.create_rectangle(self.x0,self.y0,self.x1,self.y1,fill=self.fill)
+        canvas.create_rectangle(self.x0, self.y0, self.x1, self.y1, fill=self.fill)
         canvas.create_text((self.x0 + self.x1) / 2, (self.y0 + self.y1) / 2, text=self.text)
 
-    def isClicked(self, x, y):
+    def is_clicked(self, x, y):
         return self.x0 < x < self.x1 and self.y0 < y < self.y1
-    
